378c6d7d48c8a28f486416326a855b685af7309c
sakku <kws1044@gmail.com>
2022-07-30 18:01:45 +0900

    refactoring init

---
+++ refactoring/README.md
@@ -0,0 +1,1 @@
+# 이 코드는 인프런 강의 중 [백기선님의 코딩으로 학습하는 리팩토링](https://www.inflearn.com/course/%EB%A6%AC%ED%8C%A9%ED%86%A0%EB%A7%81/dashboard) 의 강의에 대한 코드입니다.
---
+++ refactoring/src/main/java/me/whiteship/refactoring/_07_divergent_change/_13_replace_conditional_with_polymorphism/PrinterMode.java
@@ -0,0 +1,5 @@
+package me.whiteship.refactoring._07_divergent_change._13_replace_conditional_with_polymorphism;
+
+public enum PrinterMode {
+    CONSOLE, CVS, MARKDOWN
+}
